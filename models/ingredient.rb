8a7968e88e1c8d63a7c2e7db3667c97a24004b82
sunmeg <atpope5@gmail.com>
2014-09-05 13:40:22 -0400

    all tests passing

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,9 @@
 class Ingredient
+  attr_reader :name
+
+  def initialize(param_hash)
+    @name = param_hash["name"]
+    @recipe_id = param_hash["recipe_id"]
+    @id = param_hash["id"]
+  end
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,57 @@
+require_relative 'ingredient'
+
 class Recipe
+  attr_reader :id, :name, :description, :instructions, :ingredients
+  def initialize(param_hash)
+    @id = param_hash["id"]
+    @name = param_hash["name"]
+    @description = param_hash["description"] || "This recipe doesn't have a description."
+    @instructions = param_hash["instructions"] || "This recipe doesn't have any instructions."
+    @ingredients = param_hash["ingredients"]
+  end
+
+  def self.db_connection
+    begin
+      connection = PG.connect(dbname: 'recipes')
+      yield(connection)
+    ensure
+      connection.close
+    end
+  end
+
+  def self.fetch_data(query)
+    result = Recipe.db_connection do |conn|
+      conn.exec(query)
+    end
+    data = result.to_a
+  end
+
+  def self.all
+    query = "SELECT * FROM recipes;"
+    results = Recipe.fetch_data(query)
+
+    recipes = []
+    results.each do |recipe|
+      new_recipe = Recipe.new(recipe)
+      recipes << new_recipe
+    end
+    recipes
+  end
+
+  def self.find(id)
+    query = "SELECT * FROM recipes WHERE id = #{id};"
+    results = Recipe.fetch_data(query).first
+    #should refactor this to include find method in ingredients?
+
+    sql = "SELECT * FROM ingredients WHERE recipe_id = #{id}"
+    ingredients = Recipe.fetch_data(sql)
+    results["ingredients"]=[]
+
+    ingredients.each do |ingredient_hash|
+      new_ingredient = Ingredient.new(ingredient_hash)
+      results["ingredients"] << new_ingredient
+    end
+
+    Recipe.new(results)
+  end
 end
